1c30344204c05c99393f9dcbf97b13ad3cc1a67b
Yordy2001 <kingyordi2001@gmail.com>
2023-01-10 20:20:32 -0500

    create socket context

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App'
+import { SocketContext, socket } from './context/socket';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SocketContext.Provider value={socket}>
+        <App />
+      </SocketContext.Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
